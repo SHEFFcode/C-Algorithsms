43f50abae4d5229a85d9b9d34c7ab644683768e6
Yuriy Shefer <sheff@sheffmachine.com>
2016-11-14 23:01:11 -0800

    C# Algos

---
+++ Algorithms/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//collections
+
+			//1. Array - you must know it's size and type
+
+			string[] ArrayOfStrings = { "Jeremy", "James", "John" };
+
+			foreach (var element in ArrayOfStrings)
+			{
+				Console.WriteLine(element);
+			}
+
+			Console.WriteLine(ArrayOfStrings.FirstOrDefault());
+
+			//2. List - can be of different sizes, but of the same type
+			List <string> stringList = new List<string>();
+
+
+		}
+	}
+}
